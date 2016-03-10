2e9190656e7623540bd169e0e6545137399b1906
Jaap de Jong <jaap.dejong@nedap.com>
2016-03-10 23:28:07 +0100

    Add lesson 10 exercise 3

---
+++ lesson 10 Pseudoclassical Subclasses/app3.js
@@ -0,0 +1,23 @@
+var Car = function(loc){
+    this.loc = loc;
+};
+Car.prototype.move = function(){
+    this.loc++;
+};
+
+var Van = function(loc){
+    Car.call(this, loc);
+};
+Van.prototype = Object.create(Car.prototype);
+Van.prototype.constructor = Van;
+Van.prototype.grab = function(){
+	/*...*/
+};
+
+var zed = new Car(3);
+zed.move();
+
+var amy = new Van(9);
+amy.move();
+amy.grab
+
