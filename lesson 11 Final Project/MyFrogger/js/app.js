38972b6cf1627abe509c48283494cf30940678ff
Jaap de Jong <jaap.dejong@nedap.com>
2016-03-12 18:35:18 +0100

    Finish final project

--- lesson 11 Final Project/MyFrogger/js/app.js
+++ lesson 11 Final Project/MyFrogger/js/app.js
@@ -10,12 +10,16 @@ var rowStep = 83;
 var minEnemyRow = 1;
 var maxEnemyRow = 3;
 
+// speed
+var speed = 30;
+
 // Enemies our player must avoid
-var Enemy = function(col, row) {
+var Enemy = function(col, row, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = col * colStep;
     this.y = row * rowStep;
+    this.speed = speed;
     
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -28,8 +32,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += dt;
+    this.x += dt * this.speed * speed;
     if (this.x >= width) this.x = 0;
+    player.checkCollision(this.x, this.y);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -40,24 +45,28 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(col, row) {
-    this.x = col * colStep;
-    this.y = row * rowStep;
-    this.direction = 'up';
+var Player = function() {
+    this.reset();
     this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.reset = function() {
+    this.x = 2 * colStep;
+    this.y = 5 * rowStep;
+    this.direction = 'up';
 };
 
 Player.prototype.update = function(dt) {
     switch(this.direction) {
-        case 'left':  this.x -= dt; break;
-        case 'right': this.x += dt; break;
-        case 'up':    this.y -= dt; break;
-        case 'down':  this.y += dt; break;
+        case 'left':  this.x -= dt * speed; break;
+        case 'right': this.x += dt * speed; break;
+        case 'up':    this.y -= dt * speed; break;
+        case 'down':  this.y += dt * speed; break;
     }
-    if (this.x < 0) this.x = width - 1;
-    if (this.x >= width) this.x = 0;
-    if (this.y < 0) this.y = height - 1;
-    if (this.y >= height) this.y = 0;
+    if (this.x < 0) this.x = 0;
+    else if (this.x >= width - colStep) this.x = width - colStep - 1;
+    if (this.y < 0) this.reset();
+    else if (this.y >= 5 * rowStep) this.y = 5 * rowStep;
 };
 
 Player.prototype.render = function() {
@@ -68,10 +77,27 @@ Player.prototype.handleInput = function(direction) {
     this.direction = direction;
 };
 
+Player.prototype.checkCollision = function(x, y) {
+    enemyCenterX = x + colStep / 2;
+    enemyCenterY = y + rowStep / 2;
+    playerCenterX = this.x + colStep / 2;
+    playerCenterY = this.y + rowStep / 2;
+    
+    dx = playerCenterX - enemyCenterX;
+    dy = playerCenterY - enemyCenterY;
+    
+    distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    collisionDistance = Math.sqrt(Math.pow(colStep, 2) + Math.pow(rowStep, 2));
+    
+    if (distance - collisionDistance < -50) {
+        this.reset();
+    }
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(0,1), new Enemy(3,1), new Enemy(1,2), new Enemy(2,2), new Enemy(4,3)];
+var allEnemies = [new Enemy(0,1,2), new Enemy(2,2,1), new Enemy(4,3,2)];
 var player = new Player(2,5);
 
 // This listens for key presses and sends the keys to your
